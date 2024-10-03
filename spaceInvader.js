67d29484387c6229cb0eb3b298cee8bc8cb0a959
RazvanEHB <razvan.tanase@student.ehb.be>
2024-10-03 11:27:00 +0200

    Removed repetitive color style lines

--- spaceInvader.js
+++ spaceInvader.js
@@ -15,72 +15,58 @@ context.beginPath();
 context.rect(75, 125, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(75, 175, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(75, 225, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(125, 175, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(125, 225, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(175, 175, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(175, 225, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(225, 225, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(225, 175, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(275, 175, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(275, 225, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(175, 75, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(225, 275, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(125, 275, 50, 50);
 context.fill();
 
-context.fillStyle = '#cf5070';
 context.beginPath();
 context.rect(275, 125, 50, 50);
 context.fill();
