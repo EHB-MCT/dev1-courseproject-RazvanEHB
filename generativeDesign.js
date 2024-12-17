0cc7e6629e394fc98fda381249e3ce278f9cb7be
RazvanEHB <razvan.tanase@student.ehb.be>
2024-12-17 16:50:01 +0100

    TAAK KLAAR Tanase Razvan

--- generativeDesign.js
+++ generativeDesign.js
@@ -4,7 +4,7 @@ import * as Noise from "./scripts/noise.js";
 
 //Credits go to "Joeddion" and the DEV1 course for the source of the perlin noise used in this project
 //Information over the radial gradient: https://developer.mozilla.org/en-US/docs/Web/CSS/gradient/radial-gradient
-//
+//Thanks to Peter Dickx for helping me with the animations and mouse interaction
 
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -19,7 +19,6 @@ let mouseY = 0;
 
 let frameCount = 0;
 let frameCountBall = 649;
-let ballMovingRight = true;
 
 let mouseMoving = false;
 let mouseTimeout;
@@ -28,6 +27,7 @@ let xb = 0;
 let yb = 0;
 
 update();
+spaceInvader();
 steelBallUpdate();
 
 function update() {
@@ -39,13 +39,20 @@ function update() {
 }
 
 function steelBallUpdate() { 
-    if (frameCountBall < 650) {
-        frameCountBall++;
-    } else if (frameCountBall > 625) {
-        frameCountBall--;
+    if (mouseMoving) {
+        if (frameCountBall < 650) {
+            frameCountBall++;
+        } else if (frameCountBall > 625) {
+            frameCountBall--;
+        }
     }
     steelBall(frameCountBall, 500);
     requestAnimationFrame(steelBallUpdate);
+}
+
+function spaceInvader() {
+    signature(1400, 550);
+    requestAnimationFrame(spaceInvader);
 }
 
 function onMouseMove() {
@@ -63,13 +70,11 @@ function background() {
     gradient.addColorStop(0, "#6f4683");
     gradient.addColorStop(0.5, "#886593");
     gradient.addColorStop(1, "#6f4683");
-
     context.fillStyle = gradient;
     context.fillRect(0, 0, width, height);
 }
 
 function steelBall(xb, yb) {
-
     const radialGradient = context.createRadialGradient(xb - 10, yb - 10, 10, xb - 10, yb - 10, 55);
     radialGradient.addColorStop(0, "#bababa");
     radialGradient.addColorStop(0.5, "#737373");
@@ -77,12 +82,11 @@ function steelBall(xb, yb) {
     radialGradient.addColorStop(0.85, "#363636");
     context.fillStyle = radialGradient;
     utils.fillCircle(xb, yb, Math.PI * 15);
-
 }
 
 function trajectory() {
     for (let i = 0; i < 8; i++) {
-        let direct = 1
+        let direct = 1;
         const gradientLine = context.createLinearGradient(300, 0, 1000, 600);
         gradientLine.addColorStop(0, "#e23e64");
         gradientLine.addColorStop(0.5, "#e69d25");
@@ -103,6 +107,8 @@ function trajectory() {
             let y = j;
 
             context.strokeRect(x, y, 1, 1);
+
+            //This piece of code creates the red / yellow gradient trajectory. Much like the black lines, this also uses perlin noise to create a smooth path for the ball.
         }
         direct++;
     }
@@ -131,6 +137,8 @@ function perlinLines() {
             let y = j;
 
             context.strokeRect(x, y, 1, 1);
+
+            //This piece of code respectively adds a linear gradient to the black perlin lines, then draws said perlin lines, and then adds a mouse interaction for when the user moves their mouse. This creates a distortion effect.
         }
         direct++;
     }
@@ -146,3 +154,71 @@ function move(e) {
     let cursorY = e.pageY;
     console.log(move);
 }
+
+function signature(xs, ys) {
+    context.fillStyle = "black";
+    context.beginPath();
+    context.rect(xs, ys, 300, 300);
+    context.fill();
+    
+    context.fillStyle = '#cf5070';
+    context.beginPath();
+    context.rect(xs + 25, ys + 75, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 25, ys + 125, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 25, ys + 175, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 75, ys + 125, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 75, ys + 175, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 125, ys + 125, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 125, ys + 175, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 175, ys + 175, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 175, ys + 125, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 225, ys + 125, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 225, ys + 175, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 125, ys + 25, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 175, ys + 225, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 75, ys + 225, 50, 50);
+    context.fill();
+    
+    context.beginPath();
+    context.rect(xs + 225, ys + 75, 50, 50);
+    context.fill();
+    }
